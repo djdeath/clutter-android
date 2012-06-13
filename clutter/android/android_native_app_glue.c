42e9ca48be763fa50598b9d4911a8613ffe6a4bd
Lionel Landwerlin <llandwerlin@gmail.com>
2012-06-13 15:32:05 +0100

    android: remove a couple of traces

--- clutter/android/android_native_app_glue.c
+++ clutter/android/android_native_app_glue.c
@@ -28,6 +28,8 @@
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "threaded_app", __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "threaded_app", __VA_ARGS__))
 
+#define NDEBUG 1
+
 /* For debug builds, always enable the debug traces in this library */
 #ifndef NDEBUG
 #  define LOGV(...)  ((void)__android_log_print(ANDROID_LOG_VERBOSE, "threaded_app", __VA_ARGS__))
